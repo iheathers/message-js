79f32d54ba002da4e2f78286f372fc9a53cb95a6
iheathers <shresthapritam7@gmail.com>
2021-06-10 00:58:15 +0545

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('form');
+const gotoBtn = document.querySelector('.goto');
+const input = document.querySelector('#message-input');
+const linkInput = document.querySelector('#link-input');
+const decodedMsg = document.querySelector('.decoded-msg');
+const formContainer = document.querySelector('.form-container');
+const linkContainer = document.querySelector('.link-container');
+const decodeContainer = document.querySelector('.decode-section');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  formContainer.classList.add('hide');
+  linkContainer.classList.remove('hide');
+
+  const encryptedMsg = btoa(input.value);
+  linkInput.value = `${window.location}#${encryptedMsg}`;
+});
+
+const copyLink = () => {
+  linkInput.select();
+  document.execCommand('copy');
+};
+
+const copyBtn = document.querySelector('.copy-btn');
+copyBtn.addEventListener('click', copyLink);
+
+gotoBtn.addEventListener('click', () => {
+  document.location = linkInput.value;
+
+  const { hash } = window.location;
+  if (hash) {
+    decodeContainer.classList.remove('hide');
+    decodedMsg.innerHTML = atob(hash.replace('#', ''));
+    linkContainer.classList.add('hide');
+  }
+});
